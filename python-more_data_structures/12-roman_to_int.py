6225825c765ae06308945be2b9469db1d044842b
Marcelo <marcelorodb@gmail.com>
2022-09-18 19:25:41 -0700

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
+def get_symbol(i):
+        if (i == 'I'):
+            return 1
+        if (i == 'V'):
+            return 5
+        if (i == 'X'):
+            return 10
+        if (i == 'L'):
+            return 50
+        if (i == 'C'):
+            return 100
+        if (i == 'D'):
+            return 500
+        if (i == 'M'):
+            return 1000
+        return -1
+
+
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_n = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_d[roman_string[j]] - 2 * \
-                        roman_d[roman_string[j - 1]]
+    total = 0
+    x = 0
+    y = len(roman_string)
+
+    while (x < y):
+        symbol1 = get_symbol(roman_string[x])
+        if (x + 1 < y):
+            symbol2 = get_symbol(roman_string[x + 1])
+
+            if (symbol1 >= symbol2):
+                total = total + symbol1
+                x = x + 1
+            else:
+                total = total + symbol2 - symbol1
+                x = x + 2
         else:
-            roman_n += roman_d[roman_string[j]]
-    return roman_n#!/usr/bin/python3
+            total = total + symbol1
+            x = x + 1
+    return total
 
