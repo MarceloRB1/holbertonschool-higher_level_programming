4cccd07b33f824bd9846d2ff34dddb276cb450ec
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2022-11-07 19:17:29 -0300

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-    import string
-    names = dir(hidden_4)
-    for name in names:
-        if name.startswith("__") is False:
-            print(f'{name}')
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
