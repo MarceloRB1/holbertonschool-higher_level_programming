01863dac3e4a7704f44124f8471c6e4dfea33a8f
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2022-11-07 19:08:27 -0300

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    nb = 0
-    for i in range(len(sys.argv) - 1):
-        nb += int(sys.argv[i + 1])
-    print(f'{nb}')
+
+    a = len(sys.argv)
+    result = 0
+
+    for i in range(1, a):
+        result = result + int(sys.argv[i])
+    print("{}".format(result))
