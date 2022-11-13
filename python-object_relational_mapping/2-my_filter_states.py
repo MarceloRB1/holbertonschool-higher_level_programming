17be569ea7eb319219101db52b9cc375f276923d
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2022-11-13 13:43:23 -0300

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" takes in arguments and displays all values in the states table of hbtn_0e_0_usa """
+""" takes in an argument and displays all values in the states table of hbtn_0e_0_usa """
 
 import MySQLdb
 import sys
