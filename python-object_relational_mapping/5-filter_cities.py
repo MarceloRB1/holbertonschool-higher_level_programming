3de8aa6e996e8a7672b317daa89b2ceb8274c58b
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2022-11-13 13:54:57 -0300

    CORRECT CASES: EMPTY AND MANY RECORDS

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,32 +1,65 @@
 #!/usr/bin/python3
-"""  takes in the name of a state as an argument and lists all cities of that state """
-
+"""
+    Module 0-select_states.py
+"""
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        print("Usage: ./<script-name>.py [username] [password] "
-              "[database] [state]")
-        sys.exit()
+class DataBase:
+    """ Create the class DataBase to connect with MySQL """
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT b.name "
-                "FROM states a "
-                "LEFT JOIN cities b "
-                "ON a.id = b.state_id "
-                "WHERE a.name = %s "
-                "ORDER BY b.id ASC",
-                (sys.argv[4], ))
-    query_rows = cur.fetchall()
-    query_len = len(query_rows)
-    for i in range(query_len):
-        if i < query_len - 1:
-            print(query_rows[i][0], end=', ')
+    def __init__(self, user, passwd, bd, port, host):
+        """ The constructor of the class """
+        self.user = user
+        self.passwd = passwd
+        self.bd = bd
+        self.port = port
+        self.host = host
+
+    def DBConection(self):
+        """ Return the conection of the BD"""
+
+        return MySQLdb.connect(
+                                host=self.host,
+                                port=self.port,
+                                user=self.user,
+                                passwd=self.passwd,
+                                db=self.bd
+                              )
+
+    def Cursor(self):
+        """ Return the cursor of the db"""
+
+        return self.DBConection().cursor()
+
+
+if __name__ == '__main__':
+
+    conection_data = sys.argv
+
+    Uname = conection_data[1]
+    Upasswd = conection_data[2]
+    BDname = conection_data[3]
+
+    input_name = conection_data[4].split(";")
+
+    DataBase_1 = DataBase(Uname, Upasswd, BDname, 3306, "localhost")
+
+    DataBase_1_conection = DataBase_1.DBConection()
+    cursor = DataBase_1.Cursor()
+
+    st_1 = "SELECT cities.name FROM states "
+    st_2 = "JOIN cities ON cities.state_id = states.id AND "
+    st_3 = "states.name = '{}'".format(input_name[0])
+
+    cursor.execute(st_1 + st_2 + st_3)
+
+    states = cursor.fetchall()
+
+    for i in range(0, len(states)):
+        if i == len(states) - 1:
+            print(states[i][0], end="")
         else:
-            print(query_rows[i][0])
-    cur.close()
-    db.close()
+            print(states[i][0], end=", ")
+    print()
